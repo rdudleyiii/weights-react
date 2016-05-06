4298083a47dce46b1deb1ca431a524c9ecca46db
Robert Dudley <robert.dudley@dillards.com>
2016-05-06 15:45:35 -0500

    first commit

---
+++ react-tutorial-master/public/scripts/tutorial1.js
@@ -0,0 +1,18 @@
+// tutorial1.js
+
+var CommentBox = React.createClass({
+	render: function() {
+		return (
+			<div className="commentBox">
+				<h1>Comments</h1>
+				<CommentList />
+				<CommentForm />
+			</div>
+		);
+	}	
+});
+
+ReactDOM.render(
+	<CommentBox />,
+	document.getElementById('content')
+);
